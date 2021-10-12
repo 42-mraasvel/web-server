ccac7e6237d97014dd2045d67696a3d0376e3749
Maarten Benjamin Raasveldt <mraasvel@student.codam.nl>
2021-10-12 13:06:58 +0200

    start on header-fields

--- includes/http_bnf.hpp
+++ includes/http_bnf.hpp
@@ -3,8 +3,11 @@
 // #define CRLF "\r\n"
 #define SEPERATORS "()<>@,;:\\\"/[]?={} \t"
 #define SUB_DELIMS "!$&'()*+,;="
-#define ALPHA "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define LOWERCASE "abcdefghijklmnopqrstuvwxyz"
+#define UPPERCASE "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define ALPHA LOWERCASE UPPERCASE
 #define DIGIT "0123456789"
+#define ALPHA_NUMERIC ALPHA DIGIT
 #define HEXDIG DIGIT "abcdefABCDEF"
 #define UNRESERVED ALPHA DIGIT "-._~"
 #define PCHAR UNRESERVED SUB_DELIMS ":@%"
