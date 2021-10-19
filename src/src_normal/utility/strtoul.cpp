ee48d1577587f33dfb48e9ed76e60d08070042ee
Maarten Raasveldt <mbraasveldt@gmail.com>
2021-10-19 15:49:47 +0200

    Updated strtol to support any base

--- src/src_normal/utility/strtoul.cpp
+++ src/src_normal/utility/strtoul.cpp
@@ -1,39 +1,40 @@
 #include <string>
+#include <cstring>
 #include "utility/utility.hpp"
 
 namespace WebservUtility
 {
 
-unsigned long strtoul(std::string const & s)
+unsigned long strtoul(std::string const & s, int base)
 {
 	unsigned long tmp;
-	if (strtoul(s, tmp) == -1)
+	if (strtoul(s, tmp, base) == -1)
 	{
 		return 0;
 	}
 	return tmp;
 }
 
-unsigned long strtoul(const char* s)
+unsigned long strtoul(const char* s, int base)
 {
 	unsigned long tmp;
-	if (strtoul(s, tmp) == -1)
+	if (strtoul(s, tmp, base) == -1)
 	{
 		return 0;
 	}
 	return tmp;
 }
 
-int strtoul(std::string const & s, unsigned long& n)
+int strtoul(std::string const & s, unsigned long& n, int base)
 {
-	return strtoul(s.c_str(), n);
+	return strtoul(s.c_str(), n, base);
 }
 
 /*
 Return: -1 on OVERFLOW
 Stores value inside n
 */
-int strtoul(const char* s, unsigned long& n)
+int strtoul(const char* s, unsigned long& n, int base)
 {
 	std::size_t i = 0;
 	while (isspace(s[i]))
@@ -44,15 +45,27 @@ int strtoul(const char* s, unsigned long& n)
 	{
 		++i;
 	}
+
+	if (base == 16 && std::strncmp(s + i, "0x", 2) == 0)
+	{
+		i += 2;
+	}
 	n = 0;
-	while (isdigit(s[i]))
+	while (isBase(s[i], base))
 	{
 		// overflow check
-		if ((n * 10 + (s[i] - '0')) < n)
+		if ((n * base + (s[i] - '0')) < n)
 		{
 			return -1;
 		}
-		n = (n * 10) + (s[i] - '0');
+		if (isdigit(s[i]))
+		{
+			n = (n * base) + (s[i] - '0');
+		}
+		else
+		{
+			n = (n * base) + (toupper(s[i]) - 'A' + 10);
+		}
 		++i;
 	}
 	return 0;
