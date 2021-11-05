87f542865541a950eb96c2ad8fc07c7700ea41ee
Aileen Chen <aileenchen@AC-MacBookPro.local>
2021-11-05 13:35:14 +0100

    PR fix - config - ULONGMAX

--- src/src_normal/config/Config.cpp
+++ src/src_normal/config/Config.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include "utility/utility.hpp"
 #include <map>
+#include <climits>
 
 Config::Config(std::string const & config_file): _file_name(config_file), _server_amount(0), _token_index(0)
 {
