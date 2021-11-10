66c6a7658df9f5785f14f0f06272ec516719b9e6
Xiongyingzi Chen <xchen@f1r5s1.codam.nl>
2021-11-10 17:39:12 +0100

    ConfigResolver - resolve host

--- src/src_normal/handler/ConfigResolver.cpp
+++ src/src_normal/handler/ConfigResolver.cpp
@@ -2,12 +2,50 @@
 #include "settings.hpp"
 #include <iostream>
 
+//TODO: to delete
+void	ConfigResolver::createConfigMap(ConfigMap & map)
+{
+	std::vector<ConfigServer *> servers;
+
+	ConfigServer*	new_server;
+	new_server = new ConfigServer;
+	new_server->addServerName("localhost");
+	new_server->addServerName("localhost.com");
+	servers.push_back(new_server);
+	new_server = new ConfigServer;
+	new_server->addServerName("*.com");
+	servers.push_back(new_server);
+	new_server = new ConfigServer;
+	new_server->addServerName("*.com.cn");
+	servers.push_back(new_server);
+	new_server = new ConfigServer;
+	new_server->addServerName("*.cn");
+	servers.push_back(new_server);
+	new_server = new ConfigServer;
+	new_server->addServerName("www.*");
+	servers.push_back(new_server);
+	new_server = new ConfigServer;
+	new_server->addServerName("www.xxx.*");
+	new_server->addServerName("");
+	servers.push_back(new_server);
+	new_server = new ConfigServer;
+	servers.push_back(new_server);
+
+	std::pair< std::string, int > address("127.0.0.1", 8080);
+	map[address] = servers;
+}
+
 void	ConfigResolver::resolution(Request const & request)
 {
-	ConfigMap	map; //TODO
+	// TODO: to delete
+	ConfigMap	map;
+	createConfigMap(map);
 
 	ServerVector	server_vector = resolveAddress(map, request.address);
 	ConfigServer*	server = resolveHost(request, server_vector);
+
+	std::cout << RED_BOLD << "Resolved server:" << RESET_COLOR << std::endl;
+	server->print();
 }
 
 ConfigResolver::ServerVector	ConfigResolver::resolveAddress(ConfigMap map, Request::Address client_address)
@@ -32,33 +70,96 @@ void	ConfigResolver::setAddress(ConfigMap const & map, Request::Address const & 
 
 ConfigServer*	ConfigResolver::resolveHost(Request const & request, ServerVector const & servers)
 {
-	bool		default_host = !request.header_fields.contains("host");;
 	std::string	host;
-	if (!default_host)
+	setHost(request, host);
+
+	ServerVector::const_iterator it_matched;
+
+	if (isMatchEmpty(host, servers, it_matched))
 	{
-		setHost(request, host);
+		std::cout << RED_BOLD << "Match Empty!" << RESET_COLOR << std::endl;
+		return *it_matched;
 	}
-	for (ServerVector::const_iterator it = servers.begin(); it != servers.end(); ++it)
+	if (!host.empty())
 	{
-		if (isHostMatched(host, (*it)->getServerNames()))
+		if (isMatchExactName(host, servers, it_matched))
 		{
-			return *it;
+			std::cout << RED_BOLD << "Match Exactly!" << RESET_COLOR << std::endl;
+			return *it_matched;
+		}
+		if (isMatchFrontWildcard(host, servers, it_matched))
+		{
+			std::cout << RED_BOLD << "Match Front!" << RESET_COLOR << std::endl;
+			return *it_matched;
+		}
+		if (isMatchBackWildcard(host, servers, it_matched))
+		{
+			std::cout << RED_BOLD << "Match Back!" << RESET_COLOR << std::endl;
+			return *it_matched;
 		}
 	}
-	return *servers.begin();
+	std::cout << RED_BOLD << "Match Default!" << RESET_COLOR << std::endl;
+	return resolveDefaultHost(servers);
 }
 
 void	ConfigResolver::setHost(Request const & request, std::string & host)
 {
-	host = request.header_fields.find("host")->second;
-	std::size_t found = host.rfind(":");
-	if (found != std::string::npos)
+	if (request.header_fields.contains("host"))
 	{
-		host.resize(found);
+		host = request.header_fields.find("host")->second;
+		std::size_t found = host.rfind(":");
+		if (found != std::string::npos)
+		{
+			host.resize(found);
+		}
 	}
 }
 
-bool	ConfigResolver::isHostMatched(std::string const & host, StringVector const & server_names)
+bool	ConfigResolver::isMatchEmpty(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched)
+{
+	if (host.empty())
+	{
+		for (it_matched = servers.begin(); it_matched != servers.end(); ++it_matched)
+		{
+			if (isServerNameEmpty((*it_matched)->getServerNames()))
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+// TODO: check how to save "": empty string? or '\0'?
+bool	ConfigResolver::isServerNameEmpty(StringVector const & server_names)
+{
+	if (server_names.empty())
+	{
+		return true;
+	}
+	for (StringVector::const_iterator it = server_names.begin(); it != server_names.end(); ++it)
+	{
+		if ((*it).empty())
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool	ConfigResolver::isMatchExactName(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched)
+{
+	for (it_matched = servers.begin(); it_matched != servers.end(); ++it_matched)
+	{
+		if (isServerNameExactMatch(host, (*it_matched)->getServerNames()))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool	ConfigResolver::isServerNameExactMatch(std::string const & host, StringVector const & server_names)
 {
 	for (StringVector::const_iterator it = server_names.begin(); it != server_names.end(); ++it)
 	{
@@ -68,4 +169,106 @@ bool	ConfigResolver::isHostMatched(std::string const & host, StringVector const 
 		}
 	}
 	return false;
+}
+
+bool	ConfigResolver::isMatchFrontWildcard(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched)
+{
+	std::string	longest_match;
+	for (ServerVector::const_iterator it = servers.begin(); it != servers.end(); ++it)
+	{
+		if (isServerNameFrontWildcardMatch(host, (*it)->getServerNames(), longest_match))
+		{
+			it_matched = it;
+		}
+	}
+	return !longest_match.empty();
+}
+
+bool	ConfigResolver::isServerNameFrontWildcardMatch(std::string const & host, StringVector const & server_names, std::string & longest_match)
+{
+	bool	is_matched = false;
+	for (StringVector::const_iterator it = server_names.begin(); it != server_names.end(); ++it)
+	{
+		if ((*it).size() > longest_match.size() && isFrontWildCard(*it) && isHostMatchFrontWildCard(host, *it))
+		{
+			longest_match = *it;
+			is_matched = true;
+		}
+	}
+	return is_matched;
+}
+
+bool	ConfigResolver::isFrontWildCard(std::string const & string)
+{
+	return string.size() >= 2 && string[0] == '*';
+}
+
+bool	ConfigResolver::isHostMatchFrontWildCard(std::string const & host, std::string const & wildcard)
+{
+	std::string	to_find_string = wildcard.substr(1);
+	std::size_t	to_find_size = to_find_string.size();
+	std::size_t	size = host.size();
+	if (size < to_find_size)
+	{
+		return false;
+	}
+	return host.compare(size - to_find_size, to_find_size, to_find_string) == 0;
+}
+
+bool	ConfigResolver::isMatchBackWildcard(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched)
+{
+	std::string	longest_match;
+	for (ServerVector::const_iterator it = servers.begin(); it != servers.end(); ++it)
+	{
+		if (isServerNameBackWildcardMatch(host, (*it)->getServerNames(), longest_match))
+		{
+			it_matched = it;
+		}
+	}
+	return !longest_match.empty();
+}
+
+bool	ConfigResolver::isServerNameBackWildcardMatch(std::string const & host, StringVector const & server_names, std::string & longest_match)
+{
+	bool	is_matched = false;
+	for (StringVector::const_iterator it = server_names.begin(); it != server_names.end(); ++it)
+	{
+		if ((*it).size() > longest_match.size() && isBackWildCard(*it) && isHostMatchBackWildCard(host, *it))
+		{
+			longest_match = *it;
+			is_matched = true;
+		}
+	}
+	return is_matched;
+}
+
+bool	ConfigResolver::isBackWildCard(std::string const & string)
+{
+	return string.size() >= 2 && string[string.size() - 1] == '*';
+}
+
+bool	ConfigResolver::isHostMatchBackWildCard(std::string const & host, std::string const & wildcard)
+{
+	std::string	to_find_string = wildcard.substr(0, wildcard.size() - 1);
+	std::size_t	to_find_size = to_find_string.size();
+	std::size_t	size = host.size();
+	if (size < to_find_size)
+	{
+		return false;
+	}
+	return host.compare(0, to_find_size, to_find_string) == 0;
+}
+
+ConfigServer*	ConfigResolver::resolveDefaultHost(ServerVector const & servers)
+{
+	/* TODO: unhide
+	for (ServerVector::const_iterator it = servers.begin(); it != servers.end(); ++it)
+	{
+		if (*it is the default server)
+		{
+			return *it;
+		}
+	}
+	*/
+	return *servers.begin();
 }
--- src/src_normal/handler/ConfigResolver.hpp
+++ src/src_normal/handler/ConfigResolver.hpp
@@ -22,6 +22,22 @@ class ConfigResolver
 		void				setAddress(ConfigMap const & map, Request::Address const & client_address, Request::Address & address);
 		ConfigServer*	resolveHost(Request const & request, ServerVector const & servers);
 		void				setHost(Request const & request, std::string & host);
-		bool				isHostMatched(std::string const & host, StringVector const & server_names);
-		
+		bool				isMatchEmpty(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched);
+		bool					isServerNameEmpty(StringVector const & server_names);
+		bool				isMatchExactName(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched);
+		bool					isServerNameExactMatch(std::string const & host, StringVector const & server_names);
+		bool				isMatchFrontWildcard(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched);
+		bool					isServerNameFrontWildcardMatch(std::string const & host, StringVector const & server_names, std::string & longest_match);
+		bool					isFrontWildCard(std::string const & string);
+		bool					isHostMatchFrontWildCard(std::string const & host, std::string const & wildcard);
+		bool				isMatchBackWildcard(std::string const & host, ServerVector const & servers, ServerVector::const_iterator & it_matched);
+		bool					isServerNameBackWildcardMatch(std::string const & host, StringVector const & server_names, std::string & longest_match);
+		bool					isBackWildCard(std::string const & string);
+		bool					isHostMatchBackWildCard(std::string const & host, std::string const & wildcard);		
+		ConfigServer*		resolveDefaultHost(ServerVector const & servers);
+		ConfigLocation*	resolveLocation();
+
+		//TODO: to delte:
+		void	createConfigMap(ConfigMap & map);
+
 };
