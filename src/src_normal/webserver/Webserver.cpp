d34c402522437f0a956f9e69fa7e51a60fb57460
Niels Van aacken <nvan-aac@f1r1s9.codam.nl>
2021-11-24 09:02:44 +0100

    Webserver init + initServer redone

--- src/src_normal/webserver/Webserver.cpp
+++ src/src_normal/webserver/Webserver.cpp
@@ -16,7 +16,6 @@ int Webserver::initServer(std::pair<std::string, int> ip_host_pair)
 		return ERR;
 	}
 	_fd_table.insertFd(new_server);
-	// }
 	return OK;
 }
 
@@ -37,39 +36,6 @@ int	Webserver::init(Config const & config)
 	}
 	return OK;
 }
-
-
-// int Webserver::initServer(ConfigServer const & conf)
-// {
-// 	ConfigServer::const_iterator port_it;
-// 	for (port_it = conf.begin(); port_it != conf.end(); ++port_it)
-// 	{
-// 		Server *new_server = new Server();
-// 		if (new_server->setupServer(*port_it) == ERR)
-// 		{
-// 			delete new_server;
-// 			return ERR;
-// 		}
-// 		_fd_table.insertFd(new_server);
-// 	}
-// 	return OK;
-// }
-
-// /*
-// TODO: close FD after failure
-// */
-// int	Webserver::init(Config const & config)
-// {
-// 	Config::const_iterator server_it;
-// 	for (server_it = config.begin(); server_it != config.end(); ++server_it)
-// 	{
-// 		if (initServer(*server_it) == ERR)
-// 		{
-// 			return ERR;
-// 		}
-// 	}
-// 	return OK;
-// }
 
 //TODO: evaluate 'ready'
 //TODO: add Multithreading
