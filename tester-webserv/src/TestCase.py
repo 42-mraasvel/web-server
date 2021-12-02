37934e008f7c7977d8b709da40ab470a3483ba72
Xiongyingzi Chen <xchen@f1r5s5.codam.nl>
2021-12-02 18:22:06 +0100

    test case draft finished by Smaarten

--- tester-webserv/src/TestCase.py
+++ tester-webserv/src/TestCase.py
@@ -1,38 +1,49 @@
+def printHeader(header):
+	if not header:
+		return
+	print(' -- header fields --')
+	for key in header:
+		print(key, ": ", header[key], sep = "")
+
+def printBody(body):
+	if not body:
+		return
+	if len(body) < 10000:
+		print(body)
+	else:
+		print('body too big to print')
+
 class Request:
 	method = str()
-	request_target = str()
+	target = str()
 	authority = str()
-	header = dict()
+	headers = dict()
 	body = str()
 
 	def print(self):
 		print(' -- Request -- ')
-		print('method:',self.method)
-		print('request_target:',self.request_target)
-		print('authority:',self.authority)
-		print('  -- header fields --')
-		for field in self.header:
-			print(field)
-		if len(self.body) < 10000:
-			print(self.body)
-		else:
-			print('body too big to print')
-
+		print('method:', self.method)
+		print('request_target:', self.target)
+		print('authority:', self.authority)
+		printHeader(self.headers)
+		printBody(self.body)
 
 class Response:
 	status_code = int()
-	header = dict()
-	body = str()
+	headers = dict()
+	expect_body = bool()
+	body = bytes()
+
+	def __init__(self):
+		self.status_code = 0
+		self.expect_body = False
 
 	def print(self):
 		print(' -- Response --')
-		print(self.status_code)
-		for field in self.header:
-			print(field)
-		if len(self.body) < 10000:
-			print(self.body)
-		else:
-			print('body too big to print')
+		print('Status Code:', self.status_code)
+		print('Expect Body:', self.expect_body)
+		printHeader(self.headers)
+		printBody(self.body)
 
 class TestCase:
 	request = Request()
--- tester-webserv/src/main.py
+++ tester-webserv/src/main.py
@@ -13,28 +13,72 @@ def exitError(message):
 	print(RED_BOLD + "ERROR:" + RESET_COLOR, message, file = sys.stderr)
 	exit(1)
 
-def SimpleTestCase():
+def simpleTestCase():
 	testcase = TestCase.TestCase()
 
 	# Request
 	testcase.request.method = 'GET'
-	testcase.request.request_target = '/'
+	testcase.request.target = '/'
 	testcase.request.authority = 'localhost:8080'
-	testcase.request.header['name'] = 'field'
+	testcase.request.headers['name'] = 'field'
 	testcase.request.body = '1234'
-	testcase.request.header['content-length'] = len(testcase.request.body)
+	testcase.request.headers['content-length'] = str(len(testcase.request.body))
 
 	# Response
 	testcase.response.status_code = 200
+	testcase.response.headers['Content-Length'] = '185'
+	testcase.response.expect_body = True
+	with open('../page_sample/index.html', 'rb') as f:
+		testcase.response.body = f.read()
 	return testcase
 
-def ExecuteTestCase(testcase):
+def sendRequest(request):
+	uri = 'http://' + request.authority + request.target
+	return requests.request(request.method, uri, headers = request.headers, data = request.body)
+
+def evaluateStatusCode(status_code, exp_code):
+	if exp_code != 0 and status_code != exp_code:
+		return False
+	return True
+
+def evaluateHeaders(headers, exp_headers):
+	for key in exp_headers:
+		if key not in headers:
+			return False
+		elif headers[key] != exp_headers[key]:
+			return False
+	return True
+
+def evaluateBody(content, exp_content, should_cmp = True):
+	if not should_cmp:
+		return True
+	return content == exp_content
+
+def evaluateResponse(response, expected):
+	return evaluateStatusCode(response.status_code, expected.status_code) \
+	and evaluateHeaders(response.headers, expected.headers) \
+	and evaluateBody(response.content, expected.body, expected.expect_body)
+
+def executeTestCase(testcase):
 	testcase.print()
+
+	response = sendRequest(testcase.request)
+
+	print(response)
+	print(response.status_code)
+	print(response.headers)
+	print(response.content)
+
+	if not evaluateResponse(response, testcase.response):
+		print("Invalid response")
+	else:
+		print("Valid response")
+	response.close()
 
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
 		exitError(USAGE_ERR_STR)
 
-	testcase = SimpleTestCase()
-	ExecuteTestCase(testcase)
+	testcase = simpleTestCase()
+	response = executeTestCase(testcase)
