9eed829d14114a7876c3f7879389c16316850c09
Aileen Chen <aileenchen@AC-MacBookPro.local>
2021-12-07 20:34:20 +0100

    add testcase.default_file

--- tester-webserv/src/GenerateTestCases.py
+++ tester-webserv/src/GenerateTestCases.py
@@ -1,23 +1,15 @@
 import ParseTestCase
-from testcase_generation import autoindex
 from testcase_generation.locationblock import notallowed
 from testcase_generation.locationblock import redirection
 from testcase_generation.locationblock import resolution
 from testcase_generation.cgi import tester42
+from testcase_generation.resolution import autoindex
+from testcase_generation.resolution import default_file
 from testcase_generation import example
 from testcase_generation import example2
 
 def generate():
 	testcases = ParseTestCase.testCaseFromFiles()
-	testcases.append(autoindex.testCaseAutoindexOnGet())
-	testcases.append(autoindex.testCaseAutoindexOnPost())
-	testcases.append(autoindex.testCaseAutoindexOnDelete())
-	testcases.append(autoindex.testCaseAutoindexOffGet())
-	testcases.append(autoindex.testCaseAutoindexOffPost())
-	testcases.append(autoindex.testCaseAutoindexOffDelete())
-	testcases.append(autoindex.testCaseAutoindexIndexGet())
-	testcases.append(autoindex.testCaseAutoindexIndexPost())
-	testcases.append(autoindex.testCaseAutoindexIndexDelete())
 	testcases.append(notallowed.testCaseGetNotAllowed())
 	testcases.append(notallowed.testCasePostNotAllowed())
 	testcases.append(notallowed.testCaseDeleteNotAllowed())
@@ -28,6 +20,20 @@ def generate():
 	testcases.append(resolution.testCaseNonExistantBlock())
 	testcases.append(tester42.testCase42Get())
 	testcases.append(tester42.testCase42Post())
+	testcases.append(autoindex.testCaseAutoindexOnGet())
+	testcases.append(autoindex.testCaseAutoindexOnPost())
+	testcases.append(autoindex.testCaseAutoindexOnDelete())
+	testcases.append(autoindex.testCaseAutoindexOffGet())
+	testcases.append(autoindex.testCaseAutoindexOffPost())
+	testcases.append(autoindex.testCaseAutoindexOffDelete())
+	testcases.append(autoindex.testCaseAutoindexIndexGet())
+	testcases.append(autoindex.testCaseAutoindexIndexPost())
+	testcases.append(autoindex.testCaseAutoindexIndexDelete())
+	testcases.append(default_file.testCaseDefaultFileGetYes())
+	testcases.append(default_file.testCaseDefaultFilePost())
+	testcases.append(default_file.testCaseDefaultFileDelete())
+	testcases.append(default_file.testCaseDefaultFileGetNo())
+	testcases.append(default_file.testCaseDefaultFileSearch())
 	testcases.append(example.testCaseFailingTestCase())
 	testcases.append(example.testCaseExampleCustomEvaluation())
 	testcases.append(example.testCaseSecondTestCase())
