40f220d852ea56d3e107ecaf4e52405eeaeb8f25
Aileen Chen <aileenchen@AC-MacBookPro.local>
2021-12-07 12:06:55 +0100

    add testcase for AutoIndex

--- tester-webserv/src/GenerateTestCases.py
+++ tester-webserv/src/GenerateTestCases.py
@@ -1,9 +1,16 @@
 import ParseTestCase
+from testcase_generation import autoindex
 from testcase_generation import example
 from testcase_generation import example2
 
 def generate():
 	testcases = ParseTestCase.testCaseFromFiles()
+	testcases.append(autoindex.testCaseInit())
+	testcases.append(autoindex.testCaseAutoindexOn())
+	testcases.append(autoindex.testCaseAutoindexOff())
+	testcases.append(autoindex.testCaseAutoindexIndex())
+	testcases.append(autoindex.testCaseAutoindexOnPost())
+	testcases.append(autoindex.testCaseAutoindexOffDelte())
 	testcases.append(example.testCaseFailingTestCase())
 	testcases.append(example.testCaseExampleCustomEvaluation())
 	testcases.append(example.testCaseSecondTestCase())
