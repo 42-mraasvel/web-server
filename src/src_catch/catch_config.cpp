eb65c08c9e86c4fb669d9f9674d14d59c5b56317
mraasvel <mbraasveldt@gmail.com>
2021-12-17 09:52:46 +0100

    recursive file searching using C++ 17 filesystem

--- src/src_catch/catch_config.cpp
+++ src/src_catch/catch_config.cpp
@@ -3,54 +3,62 @@
 #include "settings.hpp"
 #include <iostream>
 #include <dirent.h>
-
-
+#include <vector>
+#include <filesystem>
 
 std::vector<std::string> getConfigs(std::string str)
 {
-    std::vector<std::string> ret;
-    DIR *dir = opendir(str.c_str());
-    struct dirent *file = readdir(dir);
-    while (file)
-    {
-        std::string tmp = str;
-        if (file->d_name[0] != '.')
-        {
-            tmp.append("/");
-            tmp.append(file->d_name);
-            ret.push_back(tmp);
-        }
-        file = readdir(dir); 
-    }    
-    closedir(dir);
-    return (ret);
+	std::vector<std::string> ret;
+	DIR *dir = opendir(str.c_str());
+	struct dirent *file = readdir(dir);
+	while (file)
+	{
+		std::string tmp = str;
+		if (file->d_name[0] != '.')
+		{
+			tmp.append("/");
+			tmp.append(file->d_name);
+			ret.push_back(tmp);
+		}
+		file = readdir(dir); 
+	}    
+	closedir(dir);
+	return (ret);
+}
+
+std::vector<std::string> getFilesRecursively(const std::string& dirname) {
+	std::vector<std::string> files;
+	for (const auto& file : std::filesystem::recursive_directory_iterator(dirname)) {
+		files.push_back(file.path());
+	}
+	return files;
 }
 
 TEST_CASE("Valid configurations", "[config]")
 {
-    std::vector<std::string> configs;
-    configs = getConfigs("src/src_catch/catch_config/valid");
-    for (std::vector<std::string>::iterator it = configs.begin(); it != configs.end(); ++it)
-    {
-        SECTION((*it))
-        {
-            Config conf(*it);
-            REQUIRE(conf.parser() == OK);
-        }
-    }
+	std::vector<std::string> configs;
+	configs = getFilesRecursively("src/src_catch/catch_config/valid");
+	for (std::vector<std::string>::iterator it = configs.begin(); it != configs.end(); ++it)
+	{
+		SECTION((*it))
+		{
+			Config conf(*it);
+			REQUIRE(conf.parser() == OK);
+		}
+	}
 }
 
 
 TEST_CASE("Invalid configurations", "[config]")
 {
-    std::vector<std::string> configs;
-    configs = getConfigs("src/src_catch/catch_config/invalid");
-    for (std::vector<std::string>::iterator it = configs.begin(); it != configs.end(); ++it)
-    {
-        SECTION(*it)
-        {
-            Config conf(*it);
-            REQUIRE(conf.parser() == ERR);
-        }
-    }
+	std::vector<std::string> configs;
+	configs = getFilesRecursively("src/src_catch/catch_config/invalid");
+	for (std::vector<std::string>::iterator it = configs.begin(); it != configs.end(); ++it)
+	{
+		SECTION(*it)
+		{
+			Config conf(*it);
+			REQUIRE(conf.parser() == ERR);
+		}
+	}
 }
