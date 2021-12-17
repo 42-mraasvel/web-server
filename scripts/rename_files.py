379e96a01f7acf8976dd3c89b640ac059dcb4852
mraasvel <mbraasveldt@gmail.com>
2021-12-17 10:49:46 +0100

    location directive language, server syntax check + ignore removal script

---
+++ scripts/rename_files.py
@@ -0,0 +1,15 @@
+import glob
+import os
+import sys
+import re
+
+PATH = sys.argv[1]
+result = [y for x in os.walk(PATH) for y in glob.glob(os.path.join(x[0], '*.conf'))]
+
+for file in result:
+	regex = "(.*)ignore_(.*\.conf)"
+	result = re.search(regex, file)
+	if result:
+		print(result.groups(1)[0] + result.groups(1)[1])
+
+# to rename: os.rename(oldpath, newpath)
