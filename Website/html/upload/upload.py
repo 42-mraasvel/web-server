05abd34593e1305bb0eefb6e9978bdaa27329c87
Maarten Raasveldt <mbraasveldt@gmail.com>
2021-12-20 12:04:15 +0100

    cgi

--- Website/html/upload/upload.py
+++ Website/html/upload/upload.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from requests_toolbelt.multipart import decoder
 
 def printHeader(header):
 	for field in header:
@@ -9,6 +10,23 @@ def printHeader(header):
 def readBody():
 	content = sys.stdin.read()
 	return content
+
+def postRequestImage(multipart_string):
+	content_type = os.getenv("CONTENT_TYPE")
+
+	x = decoder.MultipartDecoder(multipart_string, content_type)
+
+	print(content_type, file = sys.stderr)
+
+	if content_type == "text/plain":
+		file = open('./test.txt', 'w')
+	else:
+		file = open('./test.bin', 'wb')
+	for part in decoder.MultipartDecoder(multipart_string, content_type).parts:
+		print(type(part.content), file = sys.stderr)
+		file.write(part.content)
+		pass
+	file.close()
 
 if __name__ == '__main__':
 	if os.environ['REQUEST_METHOD'] != 'POST':
