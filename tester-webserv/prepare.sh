24be8a62d480d13b36791d94376ced907dd4cf68
Aileen Chen <aileenchen@AC-MacBookPro.local>
2021-12-21 17:56:07 +0100

    improve file preparing for tester

--- tester-webserv/prepare.sh
+++ tester-webserv/prepare.sh
@@ -7,10 +7,6 @@ GET_DIR=$METHOD_DIR'/Get'
 POST_DIR=$METHOD_DIR'/Post'
 DELETE_DIR=$METHOD_DIR'/Delete'
 
-#content to be removed each run
-TEMP_DIR=$POST_DIR'/upload'
-TEMP_DIR+=' '$POST_DIR'/root'
-
 #files
 METHOD_SAMPLE_FILE=$GET_DIR'/sample.html'
 FORBIDDEN_FILE=$FORBIDDEN_DIR'/forbidden.html'
@@ -24,9 +20,6 @@ make > /dev/null
 
 ####################################################
 
-#remove temp
-rm -rf $TEMP_DIR
-
 #build all Method directory
 mkdir -p $DELETE_DIR
 mkdir -p $FORBIDDEN_DIR
@@ -35,25 +28,18 @@ mkdir -p $POST_DIR
 #build for Method/Delete
 cp $METHOD_SAMPLE_FILE $DELETE_DIR
 
-#(re)build forbidden file
-if [ -f "$FORBIDDEN_FILE" ]; then
-	chmod 755 $FORBIDDEN_FILE
-	rm -f $FORBIDDEN_FILE
+#build forbidden file
+if [ ! -f "$FORBIDDEN_FILE" ]; then
+	cp $METHOD_SAMPLE_FILE $FORBIDDEN_FILE
+	chmod a=r $FORBIDDEN_FILE
 fi
-cp $METHOD_SAMPLE_FILE $FORBIDDEN_FILE
-chmod a=r $FORBIDDEN_FILE
 
-#(re)build forbidden sub directory
-if [ -d "$FORBIDDEN_SUBDIR" ]; then
+#build forbidden sub directory
+if [ ! -d "$FORBIDDEN_SUBDIR" ]; then
+	mkdir -p $FORBIDDEN_SUBDIR
+	cp $METHOD_SAMPLE_FILE $FORBIDDEN_SUBDIR'/sample.html'
+	chmod a=r $FORBIDDEN_SUBDIR
 	if [ "$AC_MAC" = "1" ]; then
-		sudo chown $USER $FORBIDDEN_SUBDIR
+		sudo chown nobody $FORBIDDEN_SUBDIR
 	fi
-	chmod 755 $FORBIDDEN_SUBDIR
-	rm -r $FORBIDDEN_SUBDIR
-fi
-mkdir -p $FORBIDDEN_SUBDIR
-cp $METHOD_SAMPLE_FILE $FORBIDDEN_SUBDIR'/sample.html'
-chmod a=r $FORBIDDEN_SUBDIR
-if [ "$AC_MAC" = "1" ]; then
-	sudo chown nobody $FORBIDDEN_SUBDIR
 fi
